4271c0b62dcab55b36b9825b2dbbc9d21b4ddc75
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2022-12-29 19:17:43 +0300

    Time: 29 ms (96.67%), Space: 13.8 MB (96.25%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,21 +1,9 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         ans=defaultdict(int)
-        result=0
-        for i in nums:
-            ans[i]+=1
+        pair=0
         
-        print(ans)
-        for key in ans:
-            if ans[key]!= 1:
-                combination=1
-                for i in range(1,ans[key]+1):
-                    combination =combination *i
-                for i in range(1,ans[key]-1):
-                    combination /=i
-                combination /=2
-                result =result+combination
-            combination=0
-            print(result)
-            result=int(result)
-        return result
+        for num in nums:
+            pair += ans[num]
+            ans[num]+=1
+        return pair
