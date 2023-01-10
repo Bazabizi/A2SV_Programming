b9e1b6a3f21da3110650a2d2f3ee22dfc06afb4f
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-01-10 18:25:29 +0300

    Time: 44 ms (83.85%), Space: 14 MB (18.63%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -1,20 +1,5 @@
 class Solution:
-    def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
-        '''
-        
-          x  y 
-          
-          x - 1     y      (-1, 0)
-          x + 1     y      (1, 0)
-          x   y - 1
-          x  y + 1
-          x - 1  y - 1
-          x + 1  y
-          
-          directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1),(-1, 1), (-1, -1)]
-          (1, 0)
-          king 
-        '''
+    def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]: 
         queenSet=set(map(tuple,queens))
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1),(-1, 1), (-1, -1)]
         ans=[]
