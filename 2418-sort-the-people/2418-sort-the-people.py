3ec9b4113ac48a32f761675cdbcea5b843e97e42
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-01-17 18:53:30 +0300

    Time: 611 ms (10.25%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -2,12 +2,24 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         size = len(heights)
         
-        for idx in range(size):
-            for idx2 in range(size-1,idx,-1):
-                if heights[idx2] > heights[idx2-1]:
-                    heights[idx2] , heights[idx2-1] = heights[idx2-1] , heights[idx2]
-                    names[idx2] , names[idx2-1] = names[idx2-1] , names[idx2]
+        #bubble sort
+        # for idx in range(size):
+        #     for idx2 in range(size-1,idx,-1):
+        #         if heights[idx2] > heights[idx2-1]:
+        #             heights[idx2] , heights[idx2-1] = heights[idx2-1] , heights[idx2]
+        #             names[idx2] , names[idx2-1] = names[idx2-1] , names[idx2]
 
+        #selection sort
+        
+        for idx in range(size):
+            minidx= idx
+            for idx2 in range(idx+1,size):
+                if heights[idx2] > heights[minidx]:
+                    minidx = idx2
+            
+            heights[idx] ,heights[minidx] =heights[minidx] ,heights[idx] 
+            names[idx] ,names[minidx] = names[minidx] ,names[idx] 
+            
         return names
         
         
