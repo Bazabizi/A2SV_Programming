320f6f8f7053d916a7272ce3afc1a5431f7a434e
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-02-06 17:56:54 +0300

    Time: 782 ms (79.17%), Space: 39 MB (85.53%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,31 +3,64 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         fast = head
+#         slow = head
+#         while fast.next and fast.next.next!= None:
+#             fast = fast.next.next
+#             slow = slow.next
+#         pre = slow
+#         if fast.next:
+#             slow = slow.next
+#             fast = fast.next
+#         cur = slow
+#         if slow.next:
+#             nex = slow.next
+#             while nex:
+#                 pre = slow
+#                 slow = nex
+#                 nex = nex.next
+#                 slow.next  = pre
+        
+#         while head!= cur:
+#             if head.val != slow.val:
+#                 return False
+#             head = head.next
+#             slow = slow.next
+        
+#         return True
+# Definition for singly-linked list.
+# class ListNode:
+#     def init(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        fast = head
+        check = True
         slow = head
-        while fast.next and fast.next.next!= None:
+        fast = head
+        while fast.next and fast.next.next:
             fast = fast.next.next
             slow = slow.next
-        pre = slow
-        if fast.next:
-            slow = slow.next
-            fast = fast.next
-        cur = slow
-        if slow.next:
-            nex = slow.next
-            while nex:
-                pre = slow
-                slow = nex
-                nex = nex.next
-                slow.next  = pre
+            
+        prev = None
+        curr = slow
         
-        while head!= cur:
-            if head.val != slow.val:
-                return False
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        
+        while prev and head:
+            
+            if head.val != prev.val:
+                check = False
+                break
+                
+            prev = prev.next
             head = head.next
-            slow = slow.next
-        
-        return True
+        return check
             
