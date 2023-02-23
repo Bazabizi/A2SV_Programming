20b8dba900ee437b4e3e0a721029a789062544b8
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-02-23 17:06:54 +0300

    Time: 563 ms (8.57%), Space: 13.9 MB (90.68%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,26 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        alphabet = set()
+        # left = 0
+        ans = 0
+        # count = 0
+        length = len(s)
+        
+        for letter in s:
+            alphabet.add(letter)
+        
+        for letter in alphabet:
+            count = 0
+            left = 0
+            for right in range(length):
+                if s[right] != letter:
+                    count += 1
+                
+                if count > k:
+                    while left < length and s[left] == letter:
+                        left +=1
+                    left += 1
+                    count -= 1
+                ans = max(ans , right-left + 1)
+            # print(ans, letter)
+        return ans
