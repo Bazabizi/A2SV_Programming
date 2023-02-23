1da15f8fe7ee079f35a6bc4bbde468054a53f8c0
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-02-23 14:06:28 +0300

    Time: 330 ms (30.74%), Space: 16.6 MB (66.44%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,15 +3,13 @@ class Solution:
         length = len(nums)
         count =  defaultdict(int)
         ans = 0
-        left = 0
         total = 0
         for right in range(length):
-            total += nums[right]
-            
+            total += nums[right]         
             if total == k:
                 ans += 1
             if total- k in count:
                 ans += count[total-k]
             count[total] += 1
-            
+     
         return ans
