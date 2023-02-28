192ca2f33b6ba086990741cc280d3152cdf2706b
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-02-28 07:30:41 +0300

    Time: 324 ms (96.81%), Space: 32.2 MB (32.97%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -3,13 +3,13 @@ class Solution:
         stack =[]
         maxVal = float('-inf')
         size = len(nums)
-        for idx in range(size-1,-1,-1):
-            if nums[idx] < maxVal:
+        for num in nums[::-1]:
+            if num < maxVal:
                 return True
             
-            while stack and stack[-1] < nums[idx]:
+            while stack and stack[-1] < num:
                 maxVal = stack.pop()
             
-            stack.append(nums[idx])
+            stack.append(num)
         
         return False
