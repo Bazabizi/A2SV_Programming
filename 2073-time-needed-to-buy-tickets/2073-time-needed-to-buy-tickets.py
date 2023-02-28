745ed62464f79cf206997bc2ab17dc50a731dc5a
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-02-28 13:21:47 +0300

    Time: 85 ms (6.09%), Space: 13.8 MB (53.69%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        queue = deque(tickets)
+        time = 0
+        size = len(tickets)
+        while queue[k]!=0:
+            if queue[0] > 0:
+                queue[0] -= 1
+                time += 1
+            if queue[k] == 0:
+                break
+            queue.append(queue[0])
+            queue.popleft()
+            k = (k-1) % size
+        
+        return time
