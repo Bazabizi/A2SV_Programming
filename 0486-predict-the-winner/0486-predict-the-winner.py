16b0f99fa2b1a9cd5365f8407c67cec67193a543
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-03-01 17:10:22 +0300

    Time: 5464 ms (10.78%), Space: 13.8 MB (98.78%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,34 @@
+class Solution:
+    def winner(self, arr, left , right , p1Turn):       
+        if p1Turn :
+            if right == left:
+                return [arr[right],0]
+            scoreLeft = self.winner(arr,left + 1 ,right , False)
+            scoreLeft[0] += arr[left]
+            scoreRight = self.winner(arr,left , right -1 , False)
+            scoreRight[0] += arr[right]
+            
+            if scoreLeft[0] >= scoreRight[0]:
+                return scoreLeft
+            else:
+                return scoreRight
+        else :
+            if right == left:
+                return [0,arr[right]]
+            scoreLeft = self.winner(arr,left + 1 ,right , True)
+            scoreLeft[1] += arr[left]
+            scoreRight = self.winner(arr,left , right -1 , True)
+            scoreRight[1] += arr[right]
+            
+            if scoreLeft[1] >= scoreRight[1]:
+                return scoreLeft
+            else:
+                return scoreRight
+        
+        
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        right = len(nums) - 1
+        score =  self.winner(nums , 0 , right, True)
+        
+        return score[0] >= score[1]
+    
