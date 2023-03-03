e29029219fc1d101ebc953371b963458b438d829
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-03-03 16:14:10 +0300

    Time: 1286 ms (33.48%), Space: 30.7 MB (28.08%) - LeetHub

---
+++ 2017-grid-game/2017-grid-game.py
@@ -0,0 +1,19 @@
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        rows = 3
+        cols = len(grid[0]) + 2
+        prefixSum = [[0 for _ in range(cols)] for __ in range(rows)]
+        
+        for c in range(cols - 2, 0 ,-1):
+            prefixSum[1][c] = prefixSum[1][c+1] + grid[0][c-1]
+        
+        for c in range(1,cols-1):
+            prefixSum[2][c] = prefixSum[2][c-1] + grid[1][c-1]
+        # print(prefixSum)
+        ans = float('inf')
+        col = 1 
+        while col < cols - 1:
+            val = max(prefixSum[1][col + 1] , prefixSum[2][col - 1] )
+            ans = min(val,ans)
+            col += 1
+        return ans
