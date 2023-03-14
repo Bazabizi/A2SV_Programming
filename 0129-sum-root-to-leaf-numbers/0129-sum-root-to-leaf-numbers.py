5a6681939c60fc844b6f83f1f66a11ffec4de4c2
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-03-14 09:01:34 +0300

    Time: 40 ms (17.03%), Space: 13.9 MB (54.57%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def traverse(self,root , num):
+        if not root:
+            return
+        num += str(root.val)
+        if not root.left and not root.right:
+            self.store.append(num)
+            return
+        self.traverse(root.left , num)
+        self.traverse(root.right , num)
+        
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        self.store = []
+        ans = 0
+        self.traverse(root , "")
+        for num in self.store:
+            ans += int(num)
+        
+        return ans
