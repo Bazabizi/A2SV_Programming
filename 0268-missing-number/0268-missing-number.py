6a5abbcd39e2cf980310736d1faaced5bffe7bb7
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-03-24 22:40:21 +0300

    Time: 133 ms (77.79%), Space: 15.2 MB (67.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        size=len(nums)
-        if min(nums) == 0:
-            for idx in range(size):
-                if nums[idx] != 0:
-                    while nums[idx] - 1 != idx and nums[idx] != 0 :
-                        nums[nums[idx] - 1] , nums[idx] = nums[idx] , nums[nums[idx] - 1]
-            
-            for num in range(1,size + 1):
-                if num != nums[num - 1]:
-                    return num
-            
-        return 0
+        ans = 0
+        length = len(nums)
+        total = sum(nums)
+        for val in range(length + 1):
+            ans += val
+        ans -= total
+        return ans
