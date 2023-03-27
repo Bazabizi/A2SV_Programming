23229174e5f2c68689418ec41dce8108a9b0c356
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-03-27 17:35:54 +0300

    Time: 43 ms (26.89%), Space: 14 MB (6.91%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findGreaterNumber(self, left, right, preorder,val):
+        for idx in range(left,right):
+            if preorder[idx] > val:
+                return idx
+        return -1
+    
+    
+    def preorder(self, left , right , preorder):
+        if left > right:
+            return None
+        root = TreeNode(preorder[left])
+        index = self.findGreaterNumber(left + 1, right + 1, preorder , root.val)
+        
+        if index == -1:
+            root.right = None
+            root.left = self.preorder(left + 1, right , preorder)
+        else:
+            root.left = self.preorder(left + 1, index - 1 , preorder)
+            root.right = self.preorder(index, right , preorder)
+        return root
+        
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        return self.preorder( 0 , len(preorder) - 1 , preorder)
+    
