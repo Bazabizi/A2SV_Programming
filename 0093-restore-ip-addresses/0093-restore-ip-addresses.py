44d83b899f24561c0408a7e20de1efdb22161604
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-03-27 20:54:05 +0300

    Time: 35 ms (76.11%), Space: 13.8 MB (98.67%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,41 @@
+class Solution:
+    def helper(self,temp):
+        if len(temp) > 4:
+            return False
+        for num in temp:
+        
+            if int(num) > 255:
+                return False
+        return True
+    
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        ans = []
+        def backtrack(idx , current , n):
+            if idx >= len(s) and n == 4:
+                check = self.helper(current)
+                if check:
+                    ans.append(".".join(current))
+                return 
+            if n > 3:
+                return
+           
+            for i in range(idx , len(s)):
+                val = s[idx : i+1]
+                num = int(val)
+                
+                if num < 10**(len(val)-1) and len(val) > 1:
+                    return
+                
+                if not current or int(current[-1]) <= 255 and int(val) <= 255 :
+                    
+                    current.append(val)
+                    backtrack(i + 1 , current, n + 1)
+                    current.pop() 
+        
+                if int(val) > 255:
+                    return
+                
+        backtrack( 0 , [] , 0)
+        return ans
+                
+            
