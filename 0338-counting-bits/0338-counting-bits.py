84e754904f81525dd80576bcb17671c7db834bb4
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-03-29 19:07:44 +0300

    Time: 278 ms (5.02%), Space: 20.7 MB (97.14%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = []
+        for num in range(n + 1):
+            count = 0
+            val = num
+            for _ in range(32):
+                if val & 1:
+                    count += 1
+                val = val >> 1
+            ans.append(count)
+        return ans
