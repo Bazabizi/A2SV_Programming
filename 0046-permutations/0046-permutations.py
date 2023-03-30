4c1a2d34b2e3aa321e958be9f2e767b0f3366425
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-03-30 19:21:16 +0300

    Time: 46 ms (31.07%), Space: 14.2 MB (16.83%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        
-        def backtrack(path,find):
+        used = 0
+        def backtrack(path):
+            nonlocal used
             if len(path) == len(nums):
                 ans.append(path[:])
                 return
             for idx in range(len(nums)):
-                if nums[idx] not in find:
-                    find.add(nums[idx])
+                if (used & ( 1 << idx))== 0:
+                    used |= 1 << idx
                     path.append(nums[idx])
-                    backtrack(path , find)
-                    find.remove(nums[idx])
+                    backtrack(path )
                     path.pop()
+                    used ^= 1 << idx
             
-        backtrack([] , set())
+        backtrack([])
         return ans
