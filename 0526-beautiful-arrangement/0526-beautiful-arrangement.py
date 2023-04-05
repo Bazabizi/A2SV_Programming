3ffce7c81309bac852fff887dee6138ccf620e4a
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-04-05 19:29:46 +0300

    Time: 2376 ms (5.17%), Space: 13.9 MB (66.19%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -5,12 +5,12 @@ class Solution:
             if len(arr)== n:
                 self.ans += 1
             for idx in range(1,n + 1):
-                if idx not in path:
+                if path & 1<<idx == 0:
                     if idx % (len(arr) + 1) ==0 or (len(arr) + 1) % idx ==0:
                         arr.append(idx)
-                        path.add(idx)
+                        path |= 1<<idx
                         backtrack(arr , path)
-                        path.remove(idx)
+                        path ^= 1<<idx
                         arr.pop()
-        backtrack([] ,set())
+        backtrack([] ,0)
         return self.ans
