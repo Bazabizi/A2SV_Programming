3af03a1340bd5840d4b6a0a580bd9f95587c6714
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-04-27 11:53:44 +0300

    Time: 80 ms (6.54%), Space: 16.9 MB (10.24%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,13 +1,10 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        locked = set([x for x in range(1 , len(rooms))])
         queue = deque([0])
         visited = set()
         while queue:
             room = queue.popleft()
             visited.add(room)
-            # if room in locked:
-            #     locked.remove(room)
             for keys in rooms[room]:
                 if keys not in visited:
                     queue.append(keys)
