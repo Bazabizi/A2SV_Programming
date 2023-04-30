ffcfb79427cb9ebbe8f9aa72779c400f53355fa7
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-04-30 09:45:33 +0300

    Time: 422 ms (82.51%), Space: 41.2 MB (47.00%) - LeetHub

---
+++ 0815-bus-routes/0815-bus-routes.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        graph = defaultdict(list)
+        for idx , arr in enumerate(routes):
+            for val in arr:
+                graph[val].append(idx)
+        
+        queue = deque()
+        visited = set()
+        for val in graph:
+            if val == source:
+                queue.append((graph[val] , 1))
+                break
+        index = set()
+        while queue:
+            if target == source:
+                return 0
+            idx , path = queue.popleft()
+            
+            for val in idx:
+                if val not in index:
+                    for route in routes[val]:
+                        if route == target:
+                            return path
+                        if route not in visited:
+                            queue.append((graph[route] ,path + 1 ))
+                            visited.add(route)
+                    index.add(val)
+            
+        return -1
