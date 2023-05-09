9c3e156f44fc737bb39c76aff851f0390174e5ec
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-05-09 19:35:45 +0300

    Time: 656 ms (77.08%), Space: 24.2 MB (16.88%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,26 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        colors = [0 for i in range(len(graph))]
+        length = len(graph)
+        ans = []
+        def dfs(vertix):
+            if colors[vertix] == 1:
+                return False
+            
+            colors[vertix] = 1
+            
+            for val in graph[vertix]:
+                if colors[val] == 2:
+                    continue
+                if not dfs(val):
+                    return False
+            
+            colors[vertix] = 2
+            return True
+        for idx in range(length):
+            if dfs(idx):
+                ans.append(idx)  
+        
+        
+        
+        return ans
