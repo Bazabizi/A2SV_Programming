8108d389c572a20febb1a51a85f3ab5e6a5f1741
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-05-09 18:24:17 +0300

    Time: 125 ms (14.41%), Space: 18.1 MB (11.77%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        indegree = defaultdict(set)
+        outdegree = defaultdict(list)
+        queue  = deque()
+        visited = set()
+        ans = []
+        for end   , start  in prerequisites:
+            indegree[end].add(start)
+            outdegree[start].append(end)
+        
+        for i in range(numCourses):
+            if i not in indegree:
+                queue.append(i)
+                visited.add(i)
+        
+        while queue:
+            course = queue.popleft()
+            ans.append(course)
+            for val in outdegree[course]:
+                indegree[val].remove(course)
+                if val not in visited and len(indegree[val]) == 0:
+                    queue.append(val)
+                    visited.add(val)
+        if len(ans) != numCourses:
+            return []
+        
+        return ans
