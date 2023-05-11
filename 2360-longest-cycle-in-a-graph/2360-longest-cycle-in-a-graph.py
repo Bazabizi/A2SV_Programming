d2283ea8a1364fbc081546493d5d51852c934e45
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-05-11 19:27:36 +0300

    Time: 2048 ms (5.39%), Space: 170.5 MB (18.18%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -22,8 +22,6 @@ class Solution:
                         continue
                     if color[val] == 1:
                         self.cycle  = max(path - time[val] + 1, self.cycle)
-                        color[vertix] = 2
-                        return
                     else:
                         dfs(val , path + 1)
 
