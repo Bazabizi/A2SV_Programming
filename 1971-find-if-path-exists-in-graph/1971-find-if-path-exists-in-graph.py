7910fbd298c718218dbf981b60ede04e2eb8f1b5
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-05-17 08:48:54 +0300

    Time: 2121 ms (47.08%), Space: 108.6 MB (53.95%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,21 +1,24 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(set)
-        for start , end in edges:
-            graph[start].add(end)
-            graph[end].add(start)
-        
-        visited = set()
-        stack = []
-        stack.append(source)
-        while stack:
-            cur = stack.pop()
-            if cur == destination:
-                return True
+        parent = { i : i for i in range(n)}
+        count = {i:1 for i in range(n)}
+        def find(child ):
+            if child == parent[child]:
+                return child
+            parent[child] = find(parent[child])
             
-            visited.add(cur)
-            for neighbor in graph[cur]:
-                if neighbor not in visited:
-                    stack.append(neighbor)
+            return parent[child]
+        def union(x , y):
+            rep_x = find(x)
+            rep_y = find(y)
+            if rep_x != rep_y:
+                if count[rep_x] > count[rep_y]:
+                    parent[rep_x] = rep_y
+                    count[rep_y] += count[rep_x]
+                else:
+                    parent[rep_y] = rep_x
+                    count[rep_x] += count[rep_y]
+        for start , end in edges:
+            union(start , end)
         
-        return False
+        return find(source) == find(destination)
