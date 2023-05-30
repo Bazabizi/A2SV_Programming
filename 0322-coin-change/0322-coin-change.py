dd5ccd2097ec86d774092063ae7ce8a4e6595dc2
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-05-30 18:21:49 +0300

    Time: 1712 ms (21.36%), Space: 28.3 MB (20.09%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,26 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = defaultdict(int)
+        coins.sort()
+        def dp(total):
+            if total == 0:
+                memo[total] = -1
+                return 0
+            if total < 0:
+                return float('inf')
+            if total in memo:
+                return memo[total] + 1
+            val =  float('inf')
+            for num in coins:
+                path = dp(total - num)
+                val = min(val , path)
+            memo[total] = val
+            return memo[total] + 1
+        
+        dp(amount)
+        if memo[amount] == float('inf'):
+            return -1
+        return memo[amount] + 1
+            
+        
+     
