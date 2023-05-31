f7110ddd2158ebdb9b1ca8329054f172b48887e1
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-05-31 18:38:34 +0300

    Time: 49 ms (26.05%), Space: 16.3 MB (21.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,19 +2,19 @@ class Solution:
     def __init__(self):
         self.memo = defaultdict(int)
         
-    def inbound(self,row , col):
-        return row > 0 and col > 0
     def uniquePaths(self, m: int, n: int) -> int:
         if m == 1 and n == 1:
             return 1
         
-        if (m , n) in self.memo:
-            return self.memo[(m , n)]
+        dp = [[0 for _ in range(n)] for i in range(m)]
+        dp[0][0]  = 1
+        for row in range(m):
+            for col in range(n):
+                if (row , col) == (0,0):
+                    continue
+                if row - 1 > -1:
+                    dp[row][col] += dp[row - 1][col]
+                if col > 0:
+                    dp[row][col] += dp[row][col - 1]
         
-        if self.inbound(m - 1 , n):
-            self.memo[(m , n)] += self.uniquePaths(m -1 , n)
-        
-        if self.inbound(m  , n - 1):
-            self.memo[(m , n)] += self.uniquePaths(m  , n - 1)
-        
-        return self.memo[(m , n)]
+        return dp[m - 1][n-1]
