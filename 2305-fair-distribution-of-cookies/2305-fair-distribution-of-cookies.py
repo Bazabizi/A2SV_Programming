5c253f5b5d05ddf34b4ecfb4a79e311dd914c98b
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-07-01 21:10:10 +0300

    Time: 61 ms (82.09%), Space: 16.3 MB (51.49%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -3,7 +3,7 @@ class Solution:
         bucket = [0] *k
         self.minimum = float('inf')
         cookies.sort(reverse = True)
-        def distributer(i,bucket):
+        def backtrack(i,bucket):
             if i >= len(cookies):
                 self.minimum = min(self.minimum , max(bucket))
                 return
@@ -12,8 +12,8 @@ class Solution:
             
             for j in range(k):
                 bucket[j] += cookies[i]
-                distributer(i + 1 , bucket)
+                backtrack(i + 1 , bucket)
                 bucket[j] -= cookies[i]
         
-        distributer(0,bucket)
+        backtrack(0,bucket)
         return self.minimum
