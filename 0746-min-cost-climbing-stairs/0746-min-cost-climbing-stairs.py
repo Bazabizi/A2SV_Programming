7273cccbd61f33677fb5fcaff25fd711196f9f39
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-07-05 09:56:09 +0300

    Time: 68 ms (83.54%), Space: 16.5 MB (64.12%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -5,7 +5,6 @@ class Solution:
         dp[0] = cost[0]
         dp[1] = cost[1]
         for idx in range(2 , size):
-            dp[idx] += min(dp[idx- 1] , dp[idx - 2])
-            dp[idx] += cost[idx]
+            dp[idx] += min(dp[idx- 1] , dp[idx - 2]) + cost[idx]
         
         return min(dp[-1] , dp[-2])
