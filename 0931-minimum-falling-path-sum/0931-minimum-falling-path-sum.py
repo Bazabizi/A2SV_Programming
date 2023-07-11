8e7e793511465b7a03b167da2e0cc077aff9aafe
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-07-11 10:45:52 +0300

    Time: 165 ms (38.04%), Space: 17.3 MB (31.06%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,5 +1,21 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        size = len(matrix)
+        
+        for row in range(size - 2 , -1 ,-1 ):
+            for col in range(size):
+                val = matrix[row + 1][col]
+                if col > 0:
+                    val = min(val , matrix[row + 1][col - 1])
+                if col < size - 1:
+                    val = min(val , matrix[row + 1][col + 1])
+                matrix[row][col] += val
+        
+        
+        return min(matrix[0])
+    
+    '''
+        #top down approach    
         memo = defaultdict(int)
         direction = [(1, -1) , (1, 0) , (1 , 1)]
         
@@ -32,3 +48,4 @@ class Solution:
             ans = min(ans , dp(0 , col))
         
         return ans
+  '''
