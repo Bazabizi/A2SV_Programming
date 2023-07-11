af2975918c79742ba9782b5c8d706ae0851b8165
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-07-11 10:46:27 +0300

    Time: 162 ms (42.25%), Space: 17.3 MB (31.06%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -14,38 +14,3 @@ class Solution:
         
         return min(matrix[0])
     
-    '''
-        #top down approach    
-        memo = defaultdict(int)
-        direction = [(1, -1) , (1, 0) , (1 , 1)]
-        
-        def inbound(row , col):
-            return 0 <= row < len(matrix) and 0 <= col < len(matrix)
-        
-        def lastrow(row):
-            return row == len(matrix) - 1
-
-        def dp(row , col):
-            if (row , col) in memo:
-                return memo[(row , col)]
-        
-            if lastrow(row):
-                return matrix[row][col]
-        
-            value = float('inf')
-            for r , c in direction:
-                r += row
-                c += col
-                if inbound(r , c):
-                    value = min(value , dp(r , c))
-            
-            memo[(row , col)] = matrix[row][col] + value
-            return memo[(row , col)]
-    
-        ans = float('inf')
-        
-        for col in range(len(matrix)):
-            ans = min(ans , dp(0 , col))
-        
-        return ans
-  '''
