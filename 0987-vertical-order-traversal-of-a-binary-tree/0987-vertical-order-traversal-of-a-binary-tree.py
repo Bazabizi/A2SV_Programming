229c3e12459bafa9a6bdca043436c0a32cd937b9
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-08-05 21:41:59 +0300

    Time: 42 ms (88.75%), Space: 16.6 MB (85.23%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -5,26 +5,38 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def traverse(self,root,column , row):
-        if not root:
-            return 
-        self.columnOrder[column].append([row ,root.val])
-        self.traverse(root.left, column - 1 , row + 1)
-        self.traverse(root.right, column + 1 , row + 1)
-    
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        self.columnOrder = defaultdict(list)
-        self.traverse(root,0, 0)
-        temp =[]
+        position = defaultdict(list)
         
-        for key , arr in self.columnOrder.items():
-            temp.append((key,sorted(arr)))
-        temp.sort()
+        def dfs(root , row , col):
+            if not root:
+                return
+            
+            position[col].append([row , root.val])
+            dfs(root.left , row + 1 , col - 1)
+            dfs(root.right , row + 1 , col + 1)
+            
+        {
+            1: [[1,2] , [1 , 3]]
+        }
+        dfs(root , 0 , 0)
+        array_to_be_sorted = []
+        for key , value in position.items():
+            
+            value.sort()
+            temp = []
+            for row , val in value:
+                temp.append(val)
+            
+            array_to_be_sorted.append([key , temp])
+        
+        
+        array_to_be_sorted.sort()
+        
         ans = []
-        for col , arr in temp:
-            x = []
-            for row, value in arr:
-                x.append(value)
-            ans.append(x)
-        return ans
+        for col , values in array_to_be_sorted:
+            ans.append(values)
         
+        return ans
+
+
