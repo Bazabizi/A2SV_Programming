488d441ea67631b2f870dfc9a979e75240c893ac
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-09-18 14:19:48 +0300

    Time: 73 ms (60.63%), Space: 17.6 MB (43.13%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -1,22 +1,20 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
-        rowLength = len(dungeon)
-        colLength = len(dungeon[0])
-        for row in range(rowLength - 1 , -1 , -1):
-            for col in range(colLength - 1 , -1 , -1):
-                value = dungeon[row][col]
-                val = float('inf')
-                if row != rowLength - 1:
-                    val = min(val , dungeon[row + 1][col])
-                    
-                if col != colLength - 1:
-                    val = min(val  , dungeon[row][col + 1])
-                
-                if val == float('inf'):
-                    val = 0
-                dungeon[row][col] = val - value
-                
-                if dungeon[row][col] < 0 :
-                    dungeon[row][col] = 0
-        # print(dungeon)
-        return dungeon[0][0] + 1
+        rows = len(dungeon)
+        cols = len(dungeon[0])
+        
+        dungeon[rows - 1][cols - 1] = max(1, 1 - dungeon[rows - 1][cols - 1])
+        
+        for row in range(rows - 2, -1, -1):
+            dungeon[row][cols - 1] = max(1, dungeon[row + 1][cols - 1] - dungeon[row][cols - 1])
+        
+        for col in range(cols - 2, -1, -1):
+            dungeon[rows - 1][col] = max(1, dungeon[rows - 1][col + 1] - dungeon[rows - 1][col])
+        
+        for row in range(rows - 2, -1, -1):
+            for col in range(cols - 2, -1, -1):
+                right = max(1, dungeon[row][col + 1] - dungeon[row][col])
+                down = max(1, dungeon[row + 1][col] - dungeon[row][col])
+                dungeon[row][col] = min(right, down)
+        
+        return dungeon[0][0]
