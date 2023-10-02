312b4ec2e9b0f000305d5394bbc02fa317143e21
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-10-02 10:04:44 +0300

    Time: 145 ms (91.09%), Space: 17.2 MB (94.09%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,17 +1,22 @@
 class Solution:
     def winnerOfGame(self, colors: str) -> bool:
-        if len(colors) < 3:
-            return False
-        maxA, maxB = 0,0
-        l, r = 0, 0
-        while r < len(colors):
-            c = colors[l]
-            while r < len(colors) and c == colors[r]:
-                r += 1
-            if c == 'A':
-                maxA += max(0, (r - l - 2))
-            if c == 'B':
-                maxB += max(0,(r - l -2) )
-            l = r
+        counta , countb = 0 , 0
+        maxA , maxB = 0 , 0
+        
+        for l in colors:
+            if l == 'A':
+                counta += 1
+                if countb > 2:
+                    maxB += countb - 2
+                countb = 0
+            if l == "B":
+                if counta > 2:
+                    maxA += counta - 2
+                counta = 0
+                countb += 1
+        if countb > 2:
+            maxB += countb - 2
+        if counta > 2:
+            maxA += counta - 2
+                
         return maxA > maxB
-            
