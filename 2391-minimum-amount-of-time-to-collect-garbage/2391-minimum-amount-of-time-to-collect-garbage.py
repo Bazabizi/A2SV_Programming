3d730f00175388e4930db2a7593c514058392dc1
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-10-12 08:23:46 +0300

    Time: 909 ms (50.52%), Space: 39.3 MB (71.35%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -0,0 +1,37 @@
+class Solution:
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        prefix = [0]
+        prefix.append(travel[0])
+        for num in travel[1:]:
+            val = prefix[-1]
+            prefix.append(val+num)
+        # print(prefix)
+        m = 0
+        g = 0
+        p = 0
+        idxm = -1
+        idxg = -1
+        idxp = -1
+        
+        for idx , trash in enumerate(garbage):
+            for l in trash:
+                if l == "M":
+                    m += 1
+                    idxm = idx
+                if l == "G":
+                    g += 1
+                    idxg = idx
+                if l == "P":
+                    p += 1
+                    idxp = idx
+        # print(idxg , idxp , idxm)
+        # print(g , p , m)
+        ans = 0
+        if idxg != -1:
+            ans += g + prefix[idxg]
+        if idxp != -1:
+            ans += p + prefix[idxp]
+        if idxm != -1:
+            ans += m + prefix[idxm]
+        
+        return ans
