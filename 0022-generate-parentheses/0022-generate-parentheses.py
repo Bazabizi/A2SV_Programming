e984c7e2649b7a73795e178e3c711fa8d663cd16
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-10-17 10:21:01 +0300

    Time: 44 ms (40.18%), Space: 16.7 MB (14.42%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        def backtrack(op , cl , path):
+            if op == cl == n:
+                ans.append("".join(path))
+            
+                return
+            if op != n:
+                path.append('(')
+                backtrack(op + 1 , cl , path)
+                path.pop()
+            if cl != n and op > cl:
+                path.append(')')
+                backtrack(op , cl + 1 , path)
+                path.pop()
+        
+        backtrack(0 , 0, [])
+        return ans
