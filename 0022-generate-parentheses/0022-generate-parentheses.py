c554128f2629bff6be9c1e0387be01751360f412
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-10-17 10:21:38 +0300

    Time: 39 ms (74.74%), Space: 16.7 MB (42.37%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -4,12 +4,14 @@ class Solution:
         def backtrack(op , cl , path):
             if op == cl == n:
                 ans.append("".join(path))
-            
                 return
+            
             if op != n:
                 path.append('(')
                 backtrack(op + 1 , cl , path)
                 path.pop()
+            if op < cl:
+                return
             if cl != n and op > cl:
                 path.append(')')
                 backtrack(op , cl + 1 , path)
