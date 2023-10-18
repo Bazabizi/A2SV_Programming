04f8cf62ddda308fe0812cf476daf46fa89e070b
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-10-18 15:30:59 +0300

    Time: 115 ms (22.80%), Space: 17.5 MB (91.54%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.py
+++ 0908-smallest-range-i/0908-smallest-range-i.py
@@ -1,7 +1,7 @@
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
         nums.sort()
-        ans = nums[0] + k - (nums[-1] -k)
+        ans = nums[-1] - k - nums[0] - k
         if nums[-1] - nums[0] < 2*k:
             return 0
-        return -ans
+        return ans
