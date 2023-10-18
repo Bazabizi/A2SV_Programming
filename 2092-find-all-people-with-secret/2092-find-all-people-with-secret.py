65395c9280374cc5e66374c42f330241d861f23f
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-10-18 11:40:59 +0300

    Time: 1784 ms (70.94%), Space: 59.6 MB (58.97%) - LeetHub

---
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -0,0 +1,44 @@
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        parent = {i:i for i in range(n)}
+        parent[firstPerson] = 0
+        def find(child):
+            while child != parent[child]:
+                parent[child] = parent[parent[child]]
+                child = parent[child]
+            
+            return child
+        
+        def union(x, y):
+            repx = find(x)
+            repy = find(y)
+            
+            if repx == 0 or repy == 0:
+                parent[repx] = parent[repy] = 0 
+            else:
+                parent[repx] = repy
+        
+        meetings.sort(key = lambda x:x[-1])
+        idx = 0
+        ans = []
+        length = len(meetings)
+        while idx < length:
+            sameMeeting = [meetings[idx]]
+            while idx < length - 1 and meetings[idx][2] == meetings[idx + 1][2]:
+                idx += 1
+                sameMeeting.append(meetings[idx])
+            
+            for x , y , time in sameMeeting:
+                union(x , y)
+            for x , y , time in sameMeeting:
+                rep = find(x)
+                if rep != 0:
+                    parent[x] = x
+                    parent[y] = y
+            idx += 1
+        
+        for i in range(n):
+            if find(i) == 0:
+                ans.append(i)
+        
+        return ans
